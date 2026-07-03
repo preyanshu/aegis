19c6396d82ae6a307a60b384069e2961551156e4
preyanshu <mishrapreyanshu@gmail.com>
2026-07-03 19:56:38 +0530

    modified:   frontend/components/Providers.tsx

--- frontend/components/Providers.tsx
+++ frontend/components/Providers.tsx
@@ -167,7 +167,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
                     showWalletLoginFirst: false,
                 },
                 embeddedWallets: {
-                    showWalletUIs: true,
+                    showWalletUIs: false,
                 },
             }}
         >
